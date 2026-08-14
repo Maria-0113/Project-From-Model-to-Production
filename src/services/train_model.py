231a47fe33bca18a330203b9a702a5a847f22a5a
Masha <basovamaria898@gmail.com>
2026-08-14 09:08:39 +0200

    show api logs

--- src/services/train_model.py
+++ src/services/train_model.py
@@ -107,7 +107,7 @@ def train_model(df, dataset_version):
     pr_auc = average_precision_score(y_test, y_prob)
 
 
-    logger.info(f"Model performance metrics:   Precision: {precision:.4f}   Recall: {recall:.4f}   F1-Score: {f1:.4f}   AUC: {auc:.4f}")
+    logger.info(f"Model performance metrics:   Precision: {precision:.4f}   Recall: {recall:.4f}   F1-Score: {f1:.4f}   AUC: {auc:.4f}   PR-AUC: {pr_auc:.4f}")
     return {
     "id": model_id,
     "trained_time": trained_time,
